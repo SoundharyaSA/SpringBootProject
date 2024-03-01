1a9604aacc5995b31cca7dfa2fbb6754d8fbf8c4
SoundharyaSA <soundharya.amarnath@gmail.com>
2024-03-01 12:47:19 +0530

    Added Spring Boot Project

---
+++ src/main/java/org/jsp/branch/SpringBootBranchApplication.java
@@ -0,0 +1,13 @@
+package org.jsp.branch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBranchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jsp/branch/SpringBootBranchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsp.branch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBranchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
